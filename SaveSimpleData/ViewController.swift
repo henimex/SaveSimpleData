c85de518ad5a23b95501feeab4e46b80ba3e1aac
Henimex-Mac <henimex@gmail.com>
2025-07-06 00:16:42 +0300

    Initial Commit

---
+++ SaveSimpleData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaveSimpleData
+//
+//  Created by Macbook on 6.07.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
