61d75332b8b68536a336a15e845aa5a34cb7f938
Henimex-Mac <henimex@gmail.com>
2025-07-06 10:52:01 +0300

    Final

--- SaveSimpleData/ViewController.swift
+++ SaveSimpleData/ViewController.swift
@@ -8,20 +8,62 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblTime: UILabel!
     
     @IBAction func btnSaveNote(_ sender: Any) {
-        lblNote.text = txtNote.text
-        lblTime.text = txtTime.text
+        let note =  "Note: \(txtNote.text!)"
+        let time = "Time: \(txtTime.text!)"
+        
+        lblTime.text = time
+        lblNote.text = note
+        
+        saveData(data: note, time: time)
+        clearEnteranceAreas()
     }
     
     @IBAction func btnDeleteNote(_ sender: Any) {
-        
+        deleteData()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        getData()
     }
     
-    func saveData(data:String){
+    func saveData(data:String, time: String){
+        UserDefaults.standard.set(data, forKey: "note")
+        UserDefaults.standard.set(time, forKey: "time")
+    }
+    
+    func getData(){
+        let savedNote = UserDefaults.standard.object(forKey: "note")
+        let savedTime = UserDefaults.standard.object(forKey: "time")
         
+        if let savedNoteString = savedNote as? String{
+            lblNote.text = savedNoteString
+        } else {
+            lblNote.text = "Note: No Saved Data"
+        }
+        
+        if let savedTimeString = savedTime as? String {
+            lblTime.text = savedTimeString
+        } else {
+            lblTime.text = "Time: No Saved Time"
+        }
+    }
+    
+    func deleteData(){
+        UserDefaults.standard.removeObject(forKey: "note")
+        UserDefaults.standard.removeObject(forKey: "time")
+        
+        clearEnteranceAreas()
+        clearLabels()
+    }
+    
+    func clearEnteranceAreas(){
+        txtNote.text = ""
+        txtTime.text = ""
+    }
+    
+    func clearLabels(){
+        lblNote.text = "Note:"
+        lblTime.text = "Time:"
     }
 }
-
